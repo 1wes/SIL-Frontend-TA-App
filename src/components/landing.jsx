c45ad039f067bda70018d7d944e364dd2404f124
1wes <okemwawes@gmail.com>
2024-01-30 14:57:58 +0300

    feat(landingPage): create landing page

---
+++ src/components/landing.jsx
@@ -0,0 +1,33 @@
+import './landing.css';
+import { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
+import albumImage from '../assets/album.jpg';
+
+import { Button } from '@mui/material';
+
+const LandingPage = () => {
+    
+    return (
+        <Fragment>
+            <main className='main-landing-page'>
+                <section className='hero-punchline'>
+                    <h1 className='punchline'>
+                        Need Your and Your Friends' Photos In One Place?.
+                    </h1>
+                    <p className='tagline'>
+                        Our Application sorts that out for you. View all photos arranged per album, per user.
+                    </p>
+                    
+                    <Link to={`/login`}>
+                        <Button variant='contained' id='log-in-btn'> Check Out Photos </Button>
+                    </Link>
+                </section>
+                <section className='hero-image'>
+                    <img src={albumImage} alt='album-image'/>
+                </section>
+            </main>
+        </Fragment>
+    )
+}
+export default LandingPage
