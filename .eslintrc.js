0614ab94e8ba81a8403b60c2614122235dc140c9
1wes <okemwawes@gmail.com>
2024-01-30 13:21:16 +0300

    chore(config): configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+    ],
+    plugins: ['react', 'react-hooks'],
+    rules: {
+      'no-console': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  };
