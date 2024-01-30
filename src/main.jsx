18305cb9aecf5e568d0711707eb2fd131f290e7d
1wes <okemwawes@gmail.com>
2024-01-30 15:53:46 +0300

    feat(landingPage): render landing page

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import LoginPage from './components/login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <HashRouter future={{ v7_startTransition: true }}>
+    <Routes>
+      <Route path='/' element={<App />} ></Route>
+      <Route path='/login' element={<LoginPage/>} ></Route>
+    </Routes>
+  </HashRouter>,
 )
