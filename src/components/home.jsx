b352932b1eac4ac901e350cb0017015b2bcb3eb2
1wes <okemwawes@gmail.com>
2024-01-31 16:03:13 +0300

    chore(home): add home page file

---
+++ src/components/home.jsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+const Users = () => {
+    
+    return (
+        <Fragment>
+            <main>
+                <div>
+                    Users page
+                </div>
+            </main>
+        </Fragment>
+    )
+}
+export default Users;
