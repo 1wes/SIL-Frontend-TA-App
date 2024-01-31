b6fbcf3d1f00f1daba098079da3acab1217f1422
1wes <okemwawes@gmail.com>
2024-01-31 16:02:17 +0300

    feat(login): add google log in feature

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,10 +1,32 @@
 import { Fragment } from "react";
+import { GoogleLogin } from "@react-oauth/google";
+
+import { useNavigate} from "react-router-dom";
+
+import './login.css'
 
 const LoginPage = () => {
+
+    const navigate = useNavigate();
+
+    const handleSuccess = (credentialResponse) => {
+    
+        navigate("/home");
+    }
     
     return (
         <Fragment>
-            Man
+            <main className="login-component">
+                <section className="login-btn">
+                    <h3>
+                        Sign in to see photos
+                    </h3>
+                    <div className="google-login-btn">
+                        <GoogleLogin onSuccess={handleSuccess} onError={() => { console.log("Login Failed") }} text="signin_with" type="standard"
+                        theme="filled_blue"  size="large" shape="pill" logo_alignment="center"/>
+                    </div>
+                </section>
+            </main>
         </Fragment>
     )
 }
