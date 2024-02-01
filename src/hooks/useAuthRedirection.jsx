7772025a9d568b23bf5816bb6817f020311de89b
1wes <okemwawes@gmail.com>
2024-02-01 14:24:32 +0300

    feat: check whether user is at login page before redirecting

--- src/hooks/useAuthRedirection.jsx
+++ src/hooks/useAuthRedirection.jsx
@@ -1,21 +1,33 @@
+import path from 'path';
 import { useEffect } from 'react';
 
 import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const useAuthRedirection = () => {
     
     const navigate = useNavigate();
+    const location = useLocation();
     
     useEffect(() => {
         
         const userCredentials = localStorage.getItem("userCredentials");
+        const pathname = location.pathname;
 
-        if (!userCredentials) {
-            navigate("/login")
+        if (pathname === '/login') {
+            
+            if (userCredentials) {
+                navigate("/home")
+            }
         } else {
-            navigate("/home");
+            
+            if (!userCredentials) {
+                navigate("/login")
+            }
         }
-    }, [navigate]);
+  
+
+    }, [navigate, location]);
 }
 
 export default useAuthRedirection;
