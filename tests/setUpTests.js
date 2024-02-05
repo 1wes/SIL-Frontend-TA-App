f15d35b06896fc7a46efeb15f8bea29524f15d5c
1wes <okemwawes@gmail.com>
2024-02-05 16:55:44 +0300

    test(setUpTests): mock useNavigate and wrap the tested component using GoogleOAuthProvider

---
+++ tests/setUpTests.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '@testing-library/react';
+import '@testing-library/jest-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+// Mocking useNavigate
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => jest.fn(),
+}));
+
+// Wrapping tests with Router and GoogleOAuthProvider
+const customRender = (ui, options) =>
+  render(
+    <Router>
+      <GoogleOAuthProvider>
+        {ui}
+      </GoogleOAuthProvider>
+    </Router>,
+    options
+  );
+
+export * from '@testing-library/react';
+export { customRender as render };
